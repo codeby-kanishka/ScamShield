1f8e1a6307f943937a0dd5c42f0d926b423a27d9
codeby-kanishka <kanishka191004@gmail.com>
2026-09-06 12:29:57 +0530

    Enable CORS for frontend backend communication

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,11 +5,13 @@ const connectDB = require("./config/db");
 const reportRoutes = require("./routes/reportRoutes");
 const authRoutes = require("./routes/authRoutes");
 const intelligenceRoutes = require("./routes/intelligenceRoutes");
-const aiRoutes = require("./routes/aiRoutes");
+const cors = require("cors");
+//const aiRoutes = require("./routes/aiRoutes");
 
 
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());//middleware 
 
@@ -19,7 +21,7 @@ connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/reports", reportRoutes);
 app.use("/api/intelligence", intelligenceRoutes);
-app.use("/api/ai", aiRoutes);
+//app.use("/api/ai", aiRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT || 3000}`);
